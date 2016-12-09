2ea951028e1e5649541791954c4e549eee3db24b
Frank Fang <FrankFang1990@gmail.com>
2016-12-09 21:29:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ var port = process.env.PORT || 8888;
 var server = http.createServer(function(request, response){
   if(request.url === '/1'){
 		
-		response.end('<title>1</title><script src="/3.css"></script>你请求1干什么？我给你一个随机数'+Math.random())		
+    response.end('<title>1</title><script src="/3.css"></script>你请求1干什么？我给你一个随机数'+Math.random())		
 		
 	}else if(request.url === '/3.css'){
 		
     response.setHeader('Content-Type','application/javascript')
-		response.end('var a = 1; alert(a);')
+    response.end('var a = 1; alert(a);')
 		
   }
 
