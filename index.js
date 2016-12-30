d37c0524161f9b089324492812b1f6abbdeb660b
frankfang <frankfang1990@gmail.com>
2016-12-30 22:01:37 +0800

    update headers

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
 var http = require('http')
 var fs = require('fs')
+var url = require('url')
 
 //console.log(Object.keys(http))
 var port = process.env.PORT || 8888;
 
 var server = http.createServer(function(request, response){
-  if(request.url === '/1'){
-		
-    response.end('<title>1</title><script src="/3.css"></script>你请求1干什么？我给你一个随机数'+Math.random())		
-		
-  }else if(request.url === '/3.css'){
-		
-    response.setHeader('Content-Type','application/javascript')
-    response.end('var a = 1; alert(a);')
-		
+
+  var temp = url.parse(request.url, true)
+  var path = temp.pathname
+  var query = temp.query
+
+  //从这里开始看，上面不要看
+
+  switch(path){
+    case  '/index.html':
+      var htmlString = fs.readFileSync('./index.html')
+      response.setHeader('Content-Type','text/html')
+      response.end(htmlString)		
+      break
+    case '/style.css':
+      var cssString = fs.readFileSync('./style.css')
+      response.setHeader('Content-Type','text/css')
+      response.end(cssString)
+      break
+    case '/main.js':
+      var functionName = query.callback
+      response.end(functionName + '("my password is yyy")')
+      break
+    case '/data.json':
+      response.setHeader('Content-Type','application/json')
+      response.end('{"name":"frank","age":18}')
+    default:
+      response.end('404')
+      break
   }
 
+  // 代码结束，下面不要看
 })
 
 server.listen(port)
---
+++ main.js
@@ -0,0 +1,3 @@
+document.querySelector("#button").onclick= function(){
+  alert("点击成功")
+}
