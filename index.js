68fac250d28835a11155e10628d93990ae50fe98
frankfang <frankfang1990@gmail.com>
2017-03-10 19:24:32 +0800

    get form

--- index.js
+++ index.js
@@ -16,25 +16,28 @@ var server = http.createServer(function (request, response) {
 
   console.log(method + ' ' + path)
 
-  if (path === '/') {  // 如果用户请求的是 / 路径
-    var string = fs.readFileSync('./index.html')  // 就读取 index.html 的内容
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')  // 设置响应头 Content-Type
-    response.end(string)   // 设置响应消息体
-  } if (path === '/style.css') {   // 如果用户请求的是 /style.css 路径
-    var string = fs.readFileSync('./style.css')
-    response.setHeader('Content-Type', 'text/css')
-    response.end(string)
-  } if (path === '/print-style.css') {  // 如果用户请求的是 /print-style.css 路径
-    var string = fs.readFileSync('./print-style.css')
-    response.setHeader('Content-Type', 'text/css')
-    response.end(string)
-  } if (path === '/main.js') {  // 如果用户请求的是 /main.js 路径
-    var string = fs.readFileSync('./main.js')
-    response.setHeader('Content-Type', 'application/javascript')
-    response.end(string)
-  } else {  // 如果上面都不是用户请求的路径
-    response.end('404')
+  if (method === 'GET') {
+    if (path === '/') {  // 如果用户请求的是 / 路径
+      var string = fs.readFileSync('./index.html')  // 就读取 index.html 的内容
+      response.setHeader('Content-Type', 'text/html;charset=utf-8')  // 设置响应头 Content-Type
+      response.end(string)   // 设置响应消息体
+    } if (path === '/style.css') {   // 如果用户请求的是 /style.css 路径
+      var string = fs.readFileSync('./style.css')
+      response.setHeader('Content-Type', 'text/css')
+      response.end(string)
+    } if (path === '/print-style.css') {  // 如果用户请求的是 /print-style.css 路径
+      var string = fs.readFileSync('./print-style.css')
+      response.setHeader('Content-Type', 'text/css')
+      response.end(string)
+    } if (path === '/main.js') {  // 如果用户请求的是 /main.js 路径
+      var string = fs.readFileSync('./main.js')
+      response.setHeader('Content-Type', 'application/javascript')
+      response.end(string)
+    } else {  // 如果上面都不是用户请求的路径
+      response.end('404')
+    }
   }
+
 
   // 代码结束，下面不要看
 })
