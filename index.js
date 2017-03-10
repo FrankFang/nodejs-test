0cbb0bb69a5416f2e665ec4ef13bcd7e204d3b8b
frankfang <frankfang1990@gmail.com>
2017-03-10 19:17:19 +0800

    格式化

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var url = require('url')
 //console.log(Object.keys(http))
 var port = process.env.PORT || 8888;
 
-var server = http.createServer(function(request, response){
+var server = http.createServer(function (request, response) {
 
   var temp = url.parse(request.url, true)
   var path = temp.pathname
@@ -14,25 +14,25 @@ var server = http.createServer(function(request, response){
 
   //从这里开始看，上面不要看
 
-  console.log(method)
+  console.log(method + ' ' + path)
 
-  if(path === '/'){  // 如果用户请求的是 / 路径
+  if (path === '/') {  // 如果用户请求的是 / 路径
     var string = fs.readFileSync('./index.html')  // 就读取 index.html 的内容
     response.setHeader('Content-Type', 'text/html;charset=utf-8')  // 设置响应头 Content-Type
     response.end(string)   // 设置响应消息体
-  }if(path === '/style.css'){   // 如果用户请求的是 /style.css 路径
+  } if (path === '/style.css') {   // 如果用户请求的是 /style.css 路径
     var string = fs.readFileSync('./style.css')
     response.setHeader('Content-Type', 'text/css')
     response.end(string)
-  }if(path === '/print-style.css'){  // 如果用户请求的是 /print-style.css 路径
+  } if (path === '/print-style.css') {  // 如果用户请求的是 /print-style.css 路径
     var string = fs.readFileSync('./print-style.css')
     response.setHeader('Content-Type', 'text/css')
     response.end(string)
-  }if(path === '/main.js'){  // 如果用户请求的是 /main.js 路径
+  } if (path === '/main.js') {  // 如果用户请求的是 /main.js 路径
     var string = fs.readFileSync('./main.js')
     response.setHeader('Content-Type', 'application/javascript')
     response.end(string)
-  }else{  // 如果上面都不是用户请求的路径
+  } else {  // 如果上面都不是用户请求的路径
     response.end('404')
   }
 
