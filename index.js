68e0113c13890300f5f262830f744be32dce5b80
Frank Fang <FrankFang1990@gmail.com>
2017-04-25 16:24:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -22,15 +22,12 @@ var server = http.createServer(function(request, response){
     var string = fs.readFileSync('./style.css')
     response.setHeader('Content-Type', 'text/css')
     response.end(string)
-  }else if(path === '/print-style.css'){  // 如果用户请求的是 /print-style.css 路径
-    var string = fs.readFileSync('./print-style.css')
-    response.setHeader('Content-Type', 'text/css')
-    response.end(string)
   }else if(path === '/main.js'){  // 如果用户请求的是 /main.js 路径
     var string = fs.readFileSync('./main.js')
     response.setHeader('Content-Type', 'application/javascript')
     response.end(string)
   }else{  // 如果上面都不是用户请求的路径
+    response.statusCode = 404
     response.setHeader('Content-Type', 'text/html;charset=utf-8')  // 设置响应头 Content-Type
     response.end('找不到对应的路径，你需要自行修改 index.js')
   }
