1f233be90ceed40e90ab5fcf3e8091c177dc8417
Frank Fang <FrankFang1990@gmail.com>
2017-04-25 19:54:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,20 +15,20 @@ var server = http.createServer(function(request, response){
   //从这里开始看，上面不要看
 
   if(path === '/'){  // 如果用户请求的是 / 路径
-    var string = fs.readFileSync('./index.html')  // 就读取 index.html 的内容
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')  // 设置响应头 Content-Type
-    response.end(string)   // 设置响应消息体
-  }else if(path === '/style.css'){   // 如果用户请求的是 /style.css 路径
+    var string = fs.readFileSync('./index.html')  
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')  
+    response.end(string)   
+  }else if(path === '/style.css'){   
     var string = fs.readFileSync('./style.css')
     response.setHeader('Content-Type', 'text/css')
     response.end(string)
-  }else if(path === '/main.js'){  // 如果用户请求的是 /main.js 路径
+  }else if(path === '/main.js'){  
     var string = fs.readFileSync('./main.js')
     response.setHeader('Content-Type', 'application/javascript')
     response.end(string)
-  }else{  // 如果上面都不是用户请求的路径
+  }else{  
     response.statusCode = 404
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')  // 设置响应头 Content-Type
+    response.setHeader('Content-Type', 'text/html;charset=utf-8') 
     response.end('找不到对应的路径，你需要自行修改 index.js')
   }
 
