4fb69a99bbfbe1a17696915a3b2c4f4eb0e7d269
frankfang <frankfang1990@gmail.com>
2017-10-13 15:23:19 +0800

    reset

--- index.js
+++ index.js
@@ -1,40 +1,51 @@
 var http = require('http')
 var fs = require('fs')
 var url = require('url')
+var port = process.argv[2]
 
-//console.log(Object.keys(http))
-var port = process.env.PORT || 8888;
+if(!port){
+  console.log('请指定端口号好不啦？\nnode index.js 8888 这样不会吗？')
+  process.exit(1)
+}
 
 var server = http.createServer(function(request, response){
-
-  var temp = url.parse(request.url, true)
-  var path = temp.pathname
-  var query = temp.query
+  var parsedUrl = url.parse(request.url, true)
+  var path = request.url 
+  var pathNoQuery = parsedUrl.pathname
+  var query = parsedUrl.query
   var method = request.method
 
-  //从这里开始看，上面不要看
+  /******** 从这里开始看，上面不要看 ************/
 
-  if(path === '/'){  // 如果用户请求的是 / 路径
-    var string = fs.readFileSync('./index.html')  
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')  
-    response.end(string)   
-  }else if(path === '/style.css'){   
-    var string = fs.readFileSync('./style.css')
-    response.setHeader('Content-Type', 'text/css')
-    response.end(string)
-  }else if(path === '/main.js'){  
-    var string = fs.readFileSync('./main.js')
-    response.setHeader('Content-Type', 'application/javascript')
-    response.end(string)
-  }else{  
-    response.statusCode = 404
-    response.setHeader('Content-Type', 'text/html;charset=utf-8') 
-    response.end('找不到对应的路径，你需要自行修改 index.js')
-  }
 
-  // 代码结束，下面不要看
-  console.log(method + ' ' + request.url)
+
+
+
+
+
+
+
+
+
+
+
+  console.log('方方说：得到 HTTP 路径\n' + path)
+  console.log('方方说：查询字符串为\n' + query)
+  console.log('方方说：不含查询字符串的路径为\n' + pathNoQuery)
+
+
+
+
+
+
+
+
+
+
+  /******** 代码结束，下面不要看 ************/
 })
 
 server.listen(port)
-console.log('监听 ' + port + ' 成功，请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
+console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
+
+
