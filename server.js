179dddeda2db898450f3e9eaf826a4df27d898f8
frankfang <frankfang1990@gmail.com>
2019-08-29 11:35:10 +0800

    add demo 1

--- server.js
+++ server.js
@@ -1,47 +1,56 @@
-var http = require('http')
-var fs = require('fs')
-var url = require('url')
-var port = process.argv[2]
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
+var port = process.argv[2];
 
-if(!port){
-  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
-  process.exit(1)
+if (!port) {
+  console.log("请指定端口号好不啦？\nnode server.js 8888 这样不会吗？");
+  process.exit(1);
 }
 
-var server = http.createServer(function(request, response){
-  var parsedUrl = url.parse(request.url, true)
-  var pathWithQuery = request.url 
-  var queryString = ''
-  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
-  var path = parsedUrl.pathname
-  var query = parsedUrl.query
-  var method = request.method
+var server = http.createServer(function(request, response) {
+  var parsedUrl = url.parse(request.url, true);
+  var pathWithQuery = request.url;
+  var queryString = "";
+  if (pathWithQuery.indexOf("?") >= 0) {
+    queryString = pathWithQuery.substring(pathWithQuery.indexOf("?"));
+  }
+  var path = parsedUrl.pathname;
+  var query = parsedUrl.query;
+  var method = request.method;
 
   /******** 从这里开始看，上面不要看 ************/
 
-  console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
+  console.log("有个傻子发请求过来啦！路径（带查询参数）为：" + pathWithQuery);
 
-  if(path === '/'){
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`二哈`)
-    response.end()
-  } else if(path === '/x'){
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/css;charset=utf-8')
-    response.write(`body{color: red;}`)
-    response.end()
+  if (path === "/") {
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "text/html;charset=utf-8");
+    response.write(`
+    <!DOCTYPE html>
+    <link rel="stylesheet" href="/style.css" >
+    <h1>你好</h1>
+    `);
+    response.end();
+  } else if (path === "/style.css") {
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "text/css;charset=utf-8");
+    response.write(`h1{color: red;}`);
+    response.end();
   } else {
-    response.statusCode = 404
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`你输入的路径不存在对应的内容`)
-    response.end()
+    response.statusCode = 404;
+    response.setHeader("Content-Type", "text/html;charset=utf-8");
+    response.write(`你访问的页面不存在`);
+    response.end();
   }
 
   /******** 代码结束，下面不要看 ************/
-})
+});
 
-server.listen(port)
-console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
-
-
+server.listen(port);
+console.log(
+  "监听 " +
+    port +
+    " 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:" +
+    port
+);
